b40b1c50a800a262a87b35e9fd2e50614e8d3bf3
Krishna Tandon <tandonkrishna1208@gmail.com>
2023-01-21 01:51:30 +0530

    Type casting

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+# integer
+print(int("12"))
+
+# float
+print(float("12.8"))
+
+# string
+print(str(17))
+
+# Other typecasting functions are:
+'''
+    ord(), hex(), oct(), typle(), list(), set(), dict() 
+'''
