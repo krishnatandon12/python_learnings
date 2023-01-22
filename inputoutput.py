1969d3835cd40398fbcde99fec840c77d838934f
Krishna Tandon <tandonkrishna1208@gmail.com>
2023-01-22 07:12:36 +0530

    Input output

---
+++ inputoutput.py
@@ -0,0 +1,7 @@
+fullName = input("Enter your fullname : ")
+print("Your full name is {0}".format(fullName))
+
+num1 = input("Enter first number : ")
+num2 = input("Enter second number : ")
+
+print(num1, num2, sep=", ")
