e176d98a58936ef8ee5f8cac1845743919a9d34c
Krishna Tandon <tandonkrishna1208@gmail.com>
2023-01-24 03:14:26 +0530

    Math and Logical Operators

---
+++ operators.py
@@ -0,0 +1,33 @@
+# Mathematical operators
+# Addition
+print(2+4)
+
+# Subtraction
+print(5-3)
+
+# Multiplication
+print(4*7)
+
+# Division
+print(35 / 5)
+
+# Logical Operators
+# AND, OR, NOT
+
+#a = True
+a = False
+b = False
+#b = False
+
+if a and b:
+    print("All true")
+else:
+    print("One is false")
+
+if a or b:
+    print("atleast one is true")
+else:
+    print("All false")
+
+if not (a) and not (b):
+    print("Not operator")
