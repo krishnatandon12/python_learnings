f7b3897fc91f5f83fff1e16ffb378c8111f9c8ad
Krishna Tandon <tandonkrishna1208@gmail.com>
2023-01-26 18:44:00 +0530

    Nested loop example.

---
+++ nested_loops.py
@@ -0,0 +1,7 @@
+# outer
+for i in range(10):
+    # inner
+    for j in range(10):
+        print(j, end=" ")
+    # Changes to next line. Try using different alternatives for going to next line.
+    print()
